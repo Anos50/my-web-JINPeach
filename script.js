113dfdacfe5154019fe4488b523474b96df03ffe
Anos50 <k.tarakorn@gmail.com>
2024-12-24 18:57:30 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,5 @@
+function openEnvelope() {
+    setTimeout(() => {
+        window.location.href = "https://example.com"; // แก้ไข URL เป็นหน้าที่คุณต้องการ
+    }, 1000); // หน่วงเวลา 1 วินาทีก่อนเปลี่ยนหน้า
+}
